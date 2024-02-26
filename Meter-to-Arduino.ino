074e7391ecc4318b53c9877c06a0b93933415847
Makara97 <kavindumakaranda@gmail.com>
2024-02-26 15:01:13 +0530

    Identified and solved the problem in bit wise shifting
    The bit wise shifter is not working when the output of the shifter is saved to a member of an array. If the output save to  just a parameter, there is no issue and save without any error.

--- Meter-to-Arduino.ino
+++ Meter-to-Arduino.ino
@@ -1,29 +1,24 @@
 #include <CRC16.h>
 #include <SoftwareSerial.h>
 #include <string.h>
+#include <stdint.h>
 
-/*only 2 parameters can be get from the Slave, when we try get more of them,
-the results become wrong one
-parameter 2 - 2259 0
-parameter 3 - 2259 18 0
-parameter 3 - 0 85596 55662*/
-
-const int rxPin = 2;
-const int txPin = 3;
+const uint8_t rxPin = 2;
+const uint8_t txPin = 3;
 SoftwareSerial rs485(rxPin, txPin); // Create RS485 software serial object
 
-const byte slave_address = 0x0A;
-const byte function_code = 0x03;
-const byte data[] = {0x00, 0x00, 0x00, 0x04};
-const byte CRCC[] = {0x00, 0x00};
+const uint8_t slave_address = 0x0A;
+const uint8_t function_code = 0x03;
+const uint8_t data[] = {0x00, 0x00, 0x00, 0x02};
+const uint8_t CRCC[] = {0x00, 0x00};
 
 void setup() {
-  Serial.begin(9600);
-  rs485.begin(9600); // Configure RS485 serial port
+  Serial.begin(19200);
+  rs485.begin(19200); // Configure RS485 serial port
 }
 
 void loop() {
-  byte frame[] = {slave_address, function_code, data[0], data[1], data[2], data[3], CRCC[0], CRCC[1]};
+  uint8_t frame[] = {slave_address, function_code, data[0], data[1], data[2], data[3], CRCC[0], CRCC[1]};
 
   CRC16 crc(0x8005, 0xFFFF, 0x0000, true, true);
   crc.add(frame, sizeof(frame)-2); // Exclude CRC bytes
@@ -32,7 +27,7 @@ void loop() {
   frame[sizeof(frame) - 1] = crcValue >> 8;
 
   Serial.print("Requrest-- ");
-  for (int i = 0; i < sizeof(frame); i++) {
+  for (uint8_t i = 0; i < sizeof(frame); i++) {
     Serial.print(frame[i], HEX);
     Serial.print(" ");
   }
@@ -45,65 +40,93 @@ void loop() {
   delay(10);
 
   //Read the response 
-  int dataIndex = 0; //starting point the buffer for response
-  byte dataBuf[64] = {0}; //buffer for the response msg
+  uint8_t dataIndex = 0; //starting point the buffer for response
+  uint8_t dataBuf[64] = {0}; //buffer for the response msg
 
   while(rs485.available()>0){
     //read the byte
-    byte dataR = rs485.read();
+    uint8_t dataR = rs485.read();
     //store the data in array
     dataBuf[dataIndex] = dataR;
     dataIndex++;
   }
   
-
   //print the stored data
   Serial.print("Received-- ");
-  for (int i = 0; i < dataIndex; i++) {
+  for (uint8_t i = 0; i < dataIndex; i++) {
     Serial.print(dataBuf[i], HEX);
     Serial.print(" ");
   }
   Serial.println();
 
-  int j = 0;
-  uint32_t result[j]={0}; 
+  uint8_t i = 0;
+  uint8_t k = dataBuf[2] / 4;
+  uint32_t result[k] = {0};
   
-  
-  int k = data[3] * 2;
-  Serial.println("Received bytes: "+String(k));
+  Serial.println(" variables k "+ String(k));
 
-  delay(100);
-
-  //int i = 3;
-  //result[0] = {(dataBuf[i]<<24) |  (dataBuf[i+1]<<16) |  (dataBuf[i+2]<<8) |  (dataBuf[i+3]) };
-  //i = 7;
-  //result[1] = {(dataBuf[i]<<24) |  (dataBuf[i+1]<<16) |  (dataBuf[i+2]<<8) |  (dataBuf[i+3]) };
-  
-  // for(int i=3; i<9; i+4){
-  //   result[j] = {(dataBuf[i]<<24) |  (dataBuf[i+1]<<16) |  (dataBuf[i+2]<<8) |  (dataBuf[i+3]) };
-  //   j++; 
-  // }
-
-  int i = 3;
-  while(i< k){
-    result[j] = {(dataBuf[i]<<24) |  (dataBuf[i+1]<<16) |  (dataBuf[i+2]<<8) |  (dataBuf[i+3]) };
-    j++;
-    i+=4;
+  Serial.println("dataBuf "+ String(dataIndex)) ;
+  uint8_t z[dataBuf[2]];
+  uint8_t q = 3;
+  uint8_t b = 0;
+  while (q < (dataIndex-2)){
+    z[b] = dataBuf[q]; 
+    Serial.print(String(dataBuf[q],HEX));
+    b++;
+    q++;
   }
+  Serial.println();
 
-
-  Serial.print("combined array-- ");
-  for (int i = 0; i < j ; i++) {
-    Serial.print(result[i]);
+  Serial.println("# of registers z "+ String(sizeof(z)));
+  
+  Serial.print("combined array before-- ");
+  for (uint8_t p = 0; p < sizeof(z) ; p++) {
+    Serial.print(z[p],HEX);
     Serial.print(" ");
   }
   Serial.println();
 
+  //  while (i < k ) {
+  // // Loop for each byte within the element
+  //   for (uint8_t j = 0; j < 4; j++) {
+  //     // Shift and combine bytes, handling potential index overflow
+  //     uint8_t index = i * 4 + j;
+  //     result[i] |= z[index] << (24 - 8 * j);
+  //   }
+  //   i++;
+  // } 
+ 
+
+  //result[0] = z[0] * 16777216 + z[1] * 65536 + z[2] * 256 + z[3];
+  //result[1] = z[4] * 16777216 + z[5] * 65536 + z[6] * 256 + z[7];
+
+  uint8_t j = 0; 
+  while (i < k ) {
+  // Loop for each byte within the element
+    result[i] = z[j] * 16777216 + z[j+1] * 65536 + z[j+2] * 256 + z[j+3];
+    j+=4;
+    i++;
+  }
+  
+  // Serial.println(result[0]);
+  // Serial.println(result[1]);
+
+  for (uint8_t p = 0; p < i ; p++) {
+    Serial.print(result[p]);
+    Serial.print(" ");
+  }
+  Serial.println();
+  
+
+  // Serial.print("combined array after-- ");
+  // for (uint8_t p = 0; p < i ; p++) {
+  //   Serial.print(result[p]);
+  //   Serial.print(" ");
+  // }
+  // Serial.println();
+
   delay(1000);
 }
-/*only 2 parameters can be get from the Slave, when we try get more of them,
-the results become wrong one
-parameter 2 - 2259 0
-parameter 3 - 2259 18 0
-parameter 3 - 0 85596 55662*/
+
+
 
