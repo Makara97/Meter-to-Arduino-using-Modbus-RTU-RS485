bb5233ae77ef489ec302ddcf5e5d81366b410993
Makara97 <kavindumakaranda@gmail.com>
2024-02-26 14:10:00 +0530

    Get the data using Arduino Modbus library
    Here, the Arduino Modbus library was used to get the data from the energy meter.

--- Meter-to-Arduino.ino
+++ Meter-to-Arduino.ino
@@ -1,58 +1,39 @@
-#include <CRC.h>
-#include <SoftwareSerial.h> 
-
-const int txPin = 1;
-const int rxPin = 0;
-SoftwareSerial rs485(txPin, rxPin); // Create RS485 software serial object
-
-const byte slave_address = 1;
-const byte function_code = 3;
-const byte data[] = {0, 0, 0, 2}; // Example data (2 bytes)
-const byte CRCC[] = {0, 0};
+#include <ArduinoModbus.h>
+#include <ArduinoRS485.h>
 
 void setup() {
-   // Begin serial communication for debugging
   Serial.begin(9600);
-  // Configure RS485 serial port
-  rs485.begin(9600);
-  pinMode(txPin, OUTPUT);
-  pinMode(rxPin, INPUT);
+
+  // start the Modbus RTU client
+  while (!ModbusRTUClient.begin(9600)) {
+    Serial.println("Failed to start Modbus RTU Client!");
+    delay(200);
+  }
 }
 
 void loop() {
-  byte frame[] = {slave_address, function_code, data[0], data[1], data[2], data[3], CRCC[1], CRCC[2]};
+  
+  // send a Holding registers read request to (slave) id 1, for 2 registers
+  if (!ModbusRTUClient.requestFrom(1, HOLDING_REGISTERS, 0x00, 2)) {
+    Serial.print("failed to read registers! ");
+    Serial.println(ModbusRTUClient.lastError());
+  } else {
+    // If the request succeeds, the sensor sends the readings, that are
+    // stored in the holding registers. The read() method can be used to
+    // get the raw temperature and the humidity values.
+    uint16_t rawtemperature = ModbusRTUClient.read();
+    uint16_t rawhumidity = ModbusRTUClient.read();
+    uint16_t rawte = ModbusRTUClient.read();
+    uint16_t rawhu = ModbusRTUClient.read();
 
-  CRC16 crc(0x8005, 0xFFFF, 0x0000, true); // CRC-16-IBM with bit-reversing
-  // Calculate CRC
-  crc.add(frame, sizeof(frame)); // Exclude CRC bytes
-  uint16_t crcValue = crc.getCRC();
-
-  // Append CRC to frame (reverse byte order)
-  frame[sizeof(frame) - 2] = crcValue & 0xFF;
-  frame[sizeof(frame) - 1] = crcValue >> 8;
-
-  //send Modbus frame over RS485
-  rs485.write(frame, sizeof(frame)); 
-
-  Serial.print("Request: ");
-  //for debug the frame
-  for (int i = 0; i < sizeof(frame); i++) {
-    Serial.print(frame[i], HEX);
-    Serial.print(" ");
-  }
-  Serial.println(" ");
-
-  Serial.println("Waiting for the response.......");
-  delay(100);
-
-  while (!rs485.available()) {  // Wait for data
-    delay(100);  // Small delay to avoid flooding loop
+    // To get the temperature in Celsius and the humidity reading as
+    // a percentage, divide the raw value by 10.0.
+    float temperature = rawtemperature / 10.0;
+    float humidity = rawhumidity / 10.0;
+    float tem = rawte / 10.0;
+    float hum = rawhu / 10.0;
+    Serial.println(String(tem)+String(" ")+String(hum));
   }
 
-  Serial.print ("Response: ");
-   res = rs485.read();
-
-  Serial.print(res);
-  Serial.println(" ");
-  delay(1000);
+  delay(500);
 }
