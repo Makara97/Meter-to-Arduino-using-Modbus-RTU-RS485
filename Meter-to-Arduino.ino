54b12ec8add30f08e8e1c230a27984650971a098
Makara97 <kavindumakaranda@gmail.com>
2024-03-21 15:14:58 +0530

    Update the bit rate

--- Meter-to-Arduino.ino
+++ Meter-to-Arduino.ino
@@ -16,14 +16,14 @@ const uint8_t rxPin = 2; // these pin number naming are same as the module pin n
 const uint8_t txPin = 3;
 SoftwareSerial rs485(rxPin, txPin); // Create RS485 software serial object
 
-const uint8_t slave_address = 0x0A; //slave device address
+const uint8_t slave_address = 0x02; //slave device address
 const uint8_t function_code = 0x03; 
 const uint8_t data[] = {0x00, 0x00, 0x00, 0x1A}; // starting register number and number of requesting registers
 const uint8_t CRCC[] = {0x00, 0x00}; //for the CRC values
 
 void setup() {
-  Serial.begin(19200);
-  rs485.begin(19200); // Configure RS485 serial port
+  Serial.begin(9600);
+  rs485.begin(9600); // Configure RS485 serial port
 }
 
 void loop() {
